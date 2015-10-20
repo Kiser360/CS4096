73438a946bb948cc3355fb631d733351369f7420
Kiser360 <dakrkb@mst.edu>
2015-10-20 17:13:52 -0500

    Design still sucks, champdata factory updated for items array and factory

--- app/scripts/factories/champBuild.js
+++ app/scripts/factories/champBuild.js
@@ -5,13 +5,19 @@ app.factory('champBuild',['$http', 'items', function($http, $items){
     self.getChampion = function(name, cb) {
         if (!self.champ.name || self.champ.name != name) {
             $http.get("champs.json").success(function(data){
-                self.champ = data;
+                self.champ.name = data.name;
+                self.champ.description = data.description;
+                self.champ.items = [];
 
-                $items.getItem('1001', function(itemData) {
-                    console.log(itemData);
-                });
-
-                cb(data);
+                for (var i = 0; i < 6; i++) {
+                    $items.getItem('1001', function(itemData) {
+                        self.champ.items.push(itemData);
+                        console.log(itemData);
+                        if (self.champ.items.length >= 6) {
+                            cb(self.champ);
+                        }
+                    });
+                }
             });
         }
         else {
