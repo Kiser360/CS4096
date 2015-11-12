91cb13473ff50017f8fd5128daa8c7c5fac4023e
BetaCygni1 <subliminal1111@gmail.com>
2015-11-11 19:47:07 -0600

    Search bar will actually search for champ build

--- app/scripts/factories/champInfo.js
+++ app/scripts/factories/champInfo.js
@@ -1,16 +1,16 @@
 app.factory('champInfo',['$http', function($http){
     var self = this;
-    self.champ = {}
+    self.champInfo = {name: "", title: "", lore: "", blurb: "", image: ""}
     
     self.getChampInfo = function(id, cb) {
         $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + id + "?champData=all&api_key=eb677857-8e3d-4c41-903d-dd4c11d20838")
             .success(function(data) {
-                self.champ.name = data.name;
-                self.champ.title = data.title;
-                self.champ.lore = data.lore;
-                self.champ.blurb = data.blurb;
-                self.champ.image = ("http://ddragon.leagueoflegends.com/cdn/5.22.1/img/champion/" + data.image.full);
-                cb(self.champ);
+                self.champInfo.name = data.name;
+                self.champInfo.title = data.title;
+                self.champInfo.lore = data.lore;
+                self.champInfo.blurb = data.blurb;
+                self.champInfo.image = ("http://ddragon.leagueoflegends.com/cdn/5.22.1/img/champion/" + data.image.full);
+                cb(self.champInfo);
             });
     };
 
